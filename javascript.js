cdba6f0d53cecbcdbee9a0ed0799059a85806a1d
Glory <gremman@sandia.gov>
2018-10-30 19:51:26 -0700

    firebase javascript file

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+//   <!-- Firebase JavaScript Link -->
+
+<script src="https://www.gstatic.com/firebasejs/5.5.6/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB3cDJ2hSq1v4MQkdBGTg9Mcdd3XAwcTj4",
+    authDomain: "fullstackeats.firebaseapp.com",
+    databaseURL: "https://fullstackeats.firebaseio.com",
+    projectId: "fullstackeats",
+    storageBucket: "fullstackeats.appspot.com",
+    messagingSenderId: "994105971812"
+  };
+  firebase.initializeApp(config);
